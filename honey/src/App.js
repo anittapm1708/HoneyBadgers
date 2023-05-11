cbce895e5a6637e889ad7836f9665553f631a74f
Anitta P Markose <anittapm1708@gmail.com>
2023-05-11 23:25:10 +0530

    final

--- honey/src/App.js
+++ honey/src/App.js
@@ -9,7 +9,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="" element={<Home/>} />
-          <Route path="/About" element={<About/>}/>
         </Routes>
       </BrowserRouter>
     </div>
